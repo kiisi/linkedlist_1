91235f01f87e6bfeed820a576a6fe0b4b87d8731
kiisi <kiisifelix06@gmail.com>
2022-10-26 04:23:46 +0100

    Lesson @

--- sll.py
+++ sll.py
@@ -25,3 +25,82 @@ class SLLNode:
 # RUN FROM CLI
 # python -i sll.py
 
+class SLL:
+    def __init__(self):
+        self.head = None
+    
+    def __repr__(self):
+        return "SLL object: head= {}".format(self.head)
+    
+    def is_empty(self):
+        """Returns True if the Linked List is empty, Otherwise, returns False"""
+        return self.head is None
+    
+    def add_front(self, new_data):
+        """Add a Node whose data is the new_data argument to the front of the Linked List"""
+        temp = SLLNode(new_data)
+        temp.set_next(self.head)
+        self.head = temp
+    
+    def size(self):
+        """Traverses the Linked List and returns an integer value representing the number of nodes in the Linked List.
+        
+        The time complexity is 0(n) because every Node in the Linked List must be visited in order to calculate the size of the Linked List
+        """
+        size = 0
+        if self.head is None:
+            return 0
+        
+        current = self.head
+        while current is not None: # While there are still Nodes left to count
+            size += 1
+            current = current.get_next()
+        
+        return size
+    
+    def search(self, data):
+        """
+        Traverses the Linked List and returns True if the data searched for is present in one of the Nodes. Otherwise in one Nodes. Otherwise, it returns False.
+        
+        The time complexity is 0(n)
+        """
+        if self.head is None:
+            return "Linked List is empty. No Nodes to search"
+        
+        current = self.head
+        while current is not None:
+            if current.get_data() == data:
+                return True
+            else:
+                current = current.get_next()
+        
+        return False
+                
+    
+    def remove(self, data):
+        """
+        Removes the first occurence of a Node that contains the data argument as its self.data variable. Returns nothing
+        
+        The time complexity is 0(n) because in the worst case we have to visit every Node before we find a the one we need to remove
+        """
+        if self.head is None:
+            return "Linked List is empty. No Nodes to remove"
+        
+        current = self.head
+        previous = None
+        found = False
+        
+        while not found:
+            if current.get_data() == data:
+                found = True
+            else:
+                if current.get_next() == None:
+                    return "A Node with that data value is not present"
+                else:
+                    previous = current
+                    current = current.get_next()
+        
+        if previous is None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
