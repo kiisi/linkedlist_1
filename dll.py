4ae00c93c0ca4eff031cbb973be1f5bd47b6b861
kiisi <kiisifelix06@gmail.com>
2022-10-27 03:16:02 +0100

    DONE With Lessons

--- dll.py
+++ dll.py
@@ -5,7 +5,7 @@ class DLLNode:
         self.previous = None
         
     def __repr__(self):
-        return "SLLNode object: data= {}".format(self.data)
+        return "<DLLNode object: data=> {}".format(self.data)
         
     def get_data(self):
         """Return the self.data attribute"""
@@ -34,3 +34,90 @@ class DLLNode:
 # RUN FROM CLI
 # python -i dll.py
 
+class DLL:
+
+    def __init__(self):
+        self.head = None
+        
+    def __repre__(self):
+        return "<DLL object: head=> {}".format(self.head)
+    
+    def is_empty(self):
+        return self.head is None
+    
+    def size(self):
+        """Traverses the Linked List and returns an integer value representing the number of nodes in the Linked List.
+        
+        The time complexity is 0(n) because every Node in the Linked List must be visited in order to calculate the size of the Linked List
+        """
+        size = 0
+        if self.head is None:
+            return 0
+        
+        current = self.head
+        while current is not None: # While there are still Nodes left to count
+            size += 1
+            current = current.get_next()
+        
+        return size
+    
+    def search(self, data):
+        """
+        Traverses the Linked List and returns True if the data searched for is present in one of the Nodes. Otherwise in one Nodes. Otherwise, it returns False.
+        
+        The time complexity is 0(n)
+        """
+        if self.head is None:
+            return "Linked List is empty. No Nodes to search"
+        
+        current = self.head
+        while current is not None:
+            if current.get_data() == data:
+                return True
+            else:
+                current = current.get_next()
+        
+        return False
+                
+    
+    def add_front(self, new_data):
+        """Add a Node whose data is the new_data argument to the front of the Linked List"""
+        temp = DLLNode(new_data)
+        temp.set_next(self.head)
+        
+        if self.head is not None:
+            self.head.set_previous(temp)
+            
+        self.head = temp
+       
+    
+    def remove(self, data):
+        """
+        Removes the first occurence of a Node that contains the data argument as its self.data attribute. Returns nothing.
+        
+        The time complexity is 0(n) because in the worst case, we have to visit every Node before finding the one we want to remove.
+        """
+        if self.head is None:
+            return "Linked List is empty, No Nodes to remove."
+        
+        current = self.head
+        found = False
+        
+        while not found:
+            if current.get_data() == data:
+                found = True
+            else:
+                if current.get_next() is None:
+                    return "A Node with that data value is not present"
+                else:
+                    current = current.get_next()
+        
+        if current.previous is None:
+            self.head = current.get_next()
+        else:
+            current.previous.set_next(current.get_next())
+            current.next.set_previous(current.get_previous())
+
+
+
+# Consider adding index() and insert() methods
